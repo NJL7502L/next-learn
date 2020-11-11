551592a1c10ed64a637fca7c244e369fca6c7489
NJL7502L <g.hana1211.tako@gmail.com>
2020-11-11 12:18:47 +0900

    tsの対応

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -4,21 +4,17 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  // この関数はサーバーサイドでのみ実行される.(クライアントで実行されることはない.) -> 直接DBにクエリを投げるようなコードも書くことができる.
-  // 開発環境では毎回のリクエストごとに実行されるけど, 本番環境ではビルド時のみの実行となる. -> DBへのクエリもビルド時のみなの？🤔
-  // getStaticPropsはページからのみexportできるらしい。(よくわからん)
-
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -27,7 +23,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>よくたべる。でんきであそぶ。しんちょくをあおる。</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -49,4 +45,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
